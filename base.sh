611a729db55e0cac671a5aa7b410321d6c22996b
Kevin Elliott <kevin@welikeinc.com>
2011-11-17 19:08:17 -0800

    added rvm

--- base.sh
+++ base.sh
@@ -3,3 +3,4 @@ export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
 source ~/.dotfiles/aliases.sh
 source ~/.dotfiles/bash_colors.sh
 source ~/.dotfiles/bash_prompt.sh
+source ~/.dotfiles/rvm.sh
