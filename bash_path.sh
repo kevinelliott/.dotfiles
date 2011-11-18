e48738977888a9bd6980e55eb56afe038023bb60
Kevin Elliott <kevin@welikeinc.com>
2011-11-17 19:53:13 -0800

    Added bash_path and fixed editor

---
+++ bash_path.sh
@@ -0,0 +1,5 @@
+[[ -d /usr/local/git/bin ]] && export PATH="/usr/local/git/bin:$PATH"
+[[ -d /opt/local/sbin ]] && export PATH="/opt/local/sbin:$PATH"
+[[ -d /opt/local/bin ]] && export PATH="/opt/local/bin:$PATH"
+[[ -d $HOME/bin ]] && export PATH="$HOME/bin:$PATH"
+
--- editor.sh
+++ editor.sh
@@ -1,6 +1,7 @@
 function use_textmate () {
-  export EDITOR="/usr/local/bin/mate -w"
-  export GEM_OPEN_EDITOR="/usr/local/bin/mate"
+  export MATE_PATH=`which mate`
+  export EDITOR="$MATE_PATH -w"
+  export GEM_OPEN_EDITOR="$MATE_PATH"
 
   function m {
     if [ "$1" != "" ]; then
