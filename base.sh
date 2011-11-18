fd0a84e3eabe95ccf1e5dd61dbecaa8423e85994
Kevin Elliott <kevin@welikeinc.com>
2011-11-17 19:06:17 -0800

    added bash colors and prompt

--- base.sh
+++ base.sh
@@ -1,3 +1,5 @@
 export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
 
 source ~/.dotfiles/aliases.sh
+source ~/.dotfiles/bash_colors.sh
+source ~/.dotfiles/bash_prompt.sh
---
+++ bash_colors.sh
@@ -0,0 +1,5 @@
+# colorize the Terminal
+export GREP_OPTIONS='--color=auto'
+export CLICOLOR=1;
+export TERM="xterm-color"
+alias ls="ls -G"
