71df57e48828c0a190ab9bdb2c001add656a1b6f
Kevin Elliott <kevin@welikeinc.com>
2011-11-23 16:06:45 -0800

    Added postgresql (from MacPorts) to the bash_path

--- bash_path.sh
+++ bash_path.sh
@@ -1,5 +1,6 @@
 [[ -d /usr/local/bin ]] && export PATH="/usr/local/bin:$PATH"
 [[ -d /opt/local/sbin ]] && export PATH="/opt/local/sbin:$PATH"
 [[ -d /opt/local/bin ]] && export PATH="/opt/local/bin:$PATH"
+[[ -d /opt/local/lib/postgresql83/bin ]] && export PATH="/opt/local/lib/postgresql83/bin:$PATH"
 [[ -d $HOME/bin ]] && export PATH="$HOME/bin:$PATH"
 
