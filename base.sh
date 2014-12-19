27a1845bc0182c46bfcb6776a033f6c3d68f1994
Kevin Elliott <kevin@welikeinc.com>
2014-12-18 18:48:04 -0800

    Move to more modern bundle no-document, as well as some other little changes.

--- base.sh
+++ base.sh
@@ -1,5 +1,7 @@
 export DF_BASE_PATH=~/.dotfiles
-export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+#export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home"
 
 source $DF_BASE_PATH/aliases.sh
 source $DF_BASE_PATH/autojump.sh
--- rails.sh
+++ rails.sh
@@ -1,2 +1,20 @@
-alias rc="rails new $1 -m https://raw.github.com/RailsApps/rails-composer/master/composer.rb"
+#!/bin/bash
 
+APPS_PATH=~/work/WeLike
+
+rails_composer() {
+	APP_NAME=$1
+	RUBY_VERSION=2.1.3
+	APP_PATH=$APPS_PATH/$APP_NAME
+	COMPOSER_URL=https://raw.github.com/RailsApps/rails-composer/master/composer.rb
+
+	mkdir $APP_PATH
+	cd $APP_PATH
+	
+	rvm use $RUBY_VERSION@$APP_NAME --ruby-version --create
+	gem install rails
+	
+	rails new . -m $COMPOSER_URL
+}
+
+alias rc="rails_composer $1"
